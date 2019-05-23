0a42b14d9450f80b56dab4f14fb06c902ce1ac02
Daniel Zotti <daniel.zotti87@gmail.com>
2019-05-23 10:11:33 +0200

    Aggiunto webpack per html e css

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import styles from './app.module';
+
+const obj = {
+    hello: 'World'
+}
+
+const {
+    hello
+} = obj
+
+console.log('hello')
--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,81 @@
-module.exports = {}
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+const isDevelopment = process.env.NODE_ENV !== 'production'
+
+module.exports = {
+    mode: isDevelopment ? 'development' : 'production',
+    module: {
+        rules: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.html$/,
+                exclude: /node_modules/,
+                use: [{
+                    loader: 'html-loader',
+                    options: {
+                        minimize: !isDevelopment
+                    }
+                }]
+            },
+            {
+                test: /\.module\.s(a|c)ss$/,
+                loader: [
+                    isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                            localIdentName: '[name]__[local]___[hash:base64:5]',
+                            camelCase: true,
+                            sourceMap: isDevelopment
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: isDevelopment
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.s(a|c)ss$/,
+                exclude: /\.module.(s(a|c)ss)$/,
+                loader: [
+                    isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: isDevelopment
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: isDevelopment ? '[name].css' : '[name].[hash].css',
+            chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css'
+        }),
+        new CleanWebpackPlugin({
+            output: 'dist'
+        })
+    ],
+    resolve: {
+        extensions: ['.js', '.scss']
+    },
+    output: {
+        filename: isDevelopment ? '[name].js' : '[name].[hash].js'
+    }
+}
