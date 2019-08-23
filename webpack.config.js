1f612b92440d3d7f34b927340b0417713bf50cfb
Daniel Zotti <daniel.zotti87@gmail.com>
2019-08-23 17:48:01 +0200

    Nuova gestione dei temi

--- webpack.config.js
+++ webpack.config.js
@@ -1,124 +1,125 @@
-const path = require('path');
+const path = require("path");
 
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-const isDevelopment = process.env.NODE_ENV !== 'production'
+const isDevelopment = process.env.NODE_ENV !== "production";
 
 module.exports = {
-    mode: isDevelopment ? 'development' : 'production',
-    entry: path.join(__dirname, '/src/index.ts'),
-    output: {
-        filename: isDevelopment ? '[name].js' : '[name].[hash].js',
-        path: path.join(__dirname, '/dist')
-    },
-    module: {
-        rules: [
-            // JS
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
-            },
-            // HTML
-            {
-                test: /\.html$/,
-                exclude: /node_modules/,
-                use: [{
-                    loader: 'html-loader',
-                    options: {
-                        minimize: !isDevelopment
-                    }
-                }]
-            },
-            // SCSS
-            {
-                test: /\.module\.scss$/,
-                loader: [
-                    isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules: true,
-                            localIdentName: '[name]__[local]___[hash:base64:5]',
-                            camelCase: true,
-                            sourceMap: isDevelopment
-                        }
-                    },
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            sourceMap: isDevelopment
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.scss$/,
-                exclude: /\.module.(scss)$/,
-                loader: [
-                    isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            sourceMap: isDevelopment
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.(gif|png|jpe?g|svg)$/i,
-                exclude: /node_modules/,
-                use: [
-                    'file-loader',
-                    {
-                        loader: 'image-webpack-loader',
-                        options: {
-                            mozjpeg: {
-                                progressive: true,
-                                quality: 65
-                            },
-                            optipng: {
-                                enabled: !isDevelopment
-                            },
-                            pngquant: {
-                                quality: '65-90',
-                                speed: 4
-                            },
-                            gifsicle: {
-                                interlaced: false
-                            },
-                            webp: {
-                                quality: 75
-                            }
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.ts$/,
-                exclude: /node_modules/,
-                loader: 'ts-loader',
-            },
+  mode: isDevelopment ? "development" : "production",
+  entry: path.join(__dirname, "/src/index.ts"),
+  output: {
+    filename: isDevelopment ? "[name].js" : "[name].[hash].js",
+    path: path.join(__dirname, "/dist")
+  },
+  module: {
+    rules: [
+      // JS
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      // HTML
+      {
+        test: /\.html$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "html-loader",
+            options: {
+              minimize: !isDevelopment
+            }
+          }
         ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: './src/index.html',
-            filename: './index.html'
-        }),
-        new MiniCssExtractPlugin({
-            filename: isDevelopment ? '[name].css' : '[name].[hash].css',
-            chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css'
-        }),
-        new CleanWebpackPlugin({
-            output: 'dist'
-        })
-    ],
-    resolve: {
-        extensions: ['.js', '.ts', '.scss', '.gif', '.png', '.jpg', '.jpeg', '.svg']
-    },
-
-}
+      },
+      // SCSS
+      {
+        test: /\.module\.scss$/,
+        loader: [
+          isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              localIdentName: "[name]__[local]___[hash:base64:5]",
+              camelCase: true,
+              sourceMap: isDevelopment
+            }
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: isDevelopment
+            }
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        exclude: /\.module.(scss)$/,
+        loader: [
+          isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: isDevelopment
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        exclude: /node_modules/,
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65
+              },
+              optipng: {
+                enabled: !isDevelopment
+              },
+              pngquant: {
+                quality: "65-90",
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false
+              },
+              webp: {
+                quality: 75
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        loader: "ts-loader"
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: isDevelopment ? "[name].css" : "[name].[hash].css",
+      chunkFilename: isDevelopment ? "[id].css" : "[id].[hash].css"
+    }),
+    new CleanWebpackPlugin({
+      output: "dist"
+    })
+  ],
+  resolve: {
+    extensions: [".js", ".ts", ".scss", ".gif", ".png", ".jpg", ".jpeg", ".svg"]
+  }
+};
